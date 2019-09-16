f4e6bc916eec39d3021b4c8bd74e0d0cbcc576ff
surajthite <suraj.thite@colorado.edu>
2019-09-15 21:49:43 -0600

    Program3 commented

--- ASSIGNMENT1/3.c
+++ ASSIGNMENT1/3.c
@@ -1,3 +1,27 @@
+/**********************************************************************************************************
+Author 1) Atharv Desai
+       2) Suraj Thite 
+
+Problem Statement 3 :
+
+Given the starting integer value 0xCAFE, perform each of these operations in
+series, that is, each operation should be performed on the result of the previous function. Print the
+results of each function to the command line (to capture as ProgramThree.out).
+
+1.Print the original input in hexadecimal
+2.Test if 3 of last 4 bits are on, and print the value in binary (along with the result of the test –
+true/false)
+3.Reverse the byte order, print the value in hexadecimal
+4.Test if 3 of last 4 bits are on, and print the value in binary (along with the result of the test –
+true/false)
+5.Rotate the value by four bits to the left, print the value in hexadecimal
+6.Test if 3 of last 4 bits are on, and print the value in binary (along with the result of the test –
+true/false)
+7.Rotate the value by eight bits to the right, print the value in hexadecimal
+8.Test if 3 of last 4 bits are on, and print the value in binary (along with the result of the test –
+true/false) 
+    
+********************************************************************************************************/
 #include <stdio.h>
 
 void print_hex(unsigned short int n);
@@ -6,40 +30,36 @@ unsigned int ReverseBytes(unsigned short int val);
 int main()
 {	
     unsigned short int x = 0xCAFE;  //Declaration of the starting variable value. 	
-    print_hex(x);
+    print_hex(x);   
     check_bin(x);
-    unsigned short int y = ReverseBytes(x);
+    unsigned short int y = ReverseBytes(x); // Reverse the bytes of the input variable
     print_hex(y);
     check_bin(y);
-    y= (y<<4 | y>>12);
+    y= (y<<4 | y>>12);            //Rotate the value by 4 bits to the left and move them to the rightmost 4 bits retaining all the input values while rotation. 
     print_hex(y);
     check_bin(y);
-    y= (y>>8 | y<<8);
+    y= (y>>8 | y<<8);           ////Rotate the value by 8 bits to the right and move them to the rightmost 8 bits retaining all the input values while rotation.
     print_hex(y);
-    check_bin(y);
-    //y= y && 0xF0;    
-    //printf("\n %X",y);
+    check_bin(y);               
     return 0;
 }
 
 void print_hex(unsigned short int n)
 {
-    printf("\n%X",n); // Print the value in hexa decimal.
+    printf("\n%X",n); // function to print the value in hexa decimal.
 }
 void check_bin(unsigned short int num)
     {   int bin[16];
-        int ct,i;
+        int ct,i;       // function to convert input value into binary array. 
         int flag=0;
-        ct=0;
-//Computation of the binary array            
+        ct=0;            
          while(num>0)
     {
         bin[15-ct]=num%2;
         num=num/2;
         ct++;
     }
-//Print the binary value 
-    printf("\nBinary value is: ");
+    printf("\nBinary value is: "); //Print the binary value
     for(i=0; i<16;i++)
        { printf("%d",bin[i]);}
     for(i=12; i<16;i++)
@@ -54,7 +74,7 @@ void check_bin(unsigned short int num)
         
 }
    
-unsigned int ReverseBytes(unsigned short int val)
+unsigned int ReverseBytes(unsigned short int val) //Function to reverse the byte order
 {
- return ((val) << 8 | (val) >> 8);
+ return ((val) << 8 | (val) >> 8);   //Swap the bytes implementing shift operators and retaining the input data using OR operator.
 }  
