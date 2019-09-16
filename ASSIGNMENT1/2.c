630e1e9013e848c6dda9f25eede6fbd0c728d2e8
atharvdesai1996 <49723941+atharvdesai1996@users.noreply.github.com>
2019-09-15 21:30:53 -0600

    Comments Included

--- ASSIGNMENT1/2.c
+++ ASSIGNMENT1/2.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-   int a[21]={66,114,117,99,101,32,83,97,121,115,32,72,105,33,7,9,50,48,49,57};
+   int a[21]={66,114,117,99,101,32,83,97,121,115,32,72,105,33,7,9,50,48,49,57};    //Taking input
    int i=0;
    int Code;
    for ( i=0;a[i]!=0;i++) {
@@ -11,25 +11,25 @@ int main(void)
       }
    for (i=0;a[i]!=0;i++){
 
-     if( islower(a[i]))
+     if( islower(a[i]))                    //to check if the ASCII char is in lower case
      {
          printf("\n Code: %d \t \t Type: Lower Case \t \t ASCII char =%c ",a[i],a[i]);
      }
-     if( isupper(a[i]))
+     if( isupper(a[i]))                    //to check if the ASCII char is in upper case
      {
          printf("\n Code: %d \t \t Type: Upper Case \t \t ASCII char =%c ",a[i],a[i]);
      }
-     if( isdigit(a[i]))
+     if( isdigit(a[i]))                    //to check if the ASCII char is a number between 0-9
      {
          printf("\n Code: %d \t \t Type: digit \t \t \t ASCII char =%c ",a[i],a[i]);
      }
-     if( ispunct(a[i]))
+     if( ispunct(a[i]))                   //to check if the ASCII char is a special character
      {
          printf("\n Code: %d \t \t Type: special char \t \t ASCII char =%c ",a[i],a[i]);
      }
-      if( isspace(a[i]) || a[i]==7)
+      if( isspace(a[i]) || a[i]==7)       //to check if the ASCII char indicates space
      {
-         printf("\n Code: %d \t \t Type: space \t \t \t ASCII char =%c ",a[i],a[i]);
+         printf("\n Code: %d \t \t Type: space \t \t \t ASCII char =space ",a[i]);
       }
 }
    printf(" \n");
