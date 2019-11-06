66b3f49639bc7486dded2eadae298358d8054775
suraj-thite <suraj.thite@colorado.edu>
2019-11-06 09:01:21 -0700

    ASSIGNMENT4 FOLDER ADDED

---
+++ ASSIGNMENT3/pattern.h
@@ -0,0 +1,22 @@
+/*
+ * pattern.h
+ *
+ *  Created on: Oct 21, 2019
+ *      Author: AKSHAY THITE
+ */
+
+#ifndef PATTERN_H_
+#define PATTERN_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+//Function to generate pattern for storing and verification purpose.
+void mem_status_write_pattern_array(uint32_t * loc, size_t length, uint8_t seed);
+void gen_pattern(uint32_t * pattern, size_t length, int8_t seed);
+
+
+
+#endif /* PATTERN_H_ */
