9c53c98e8d533413b3dc6968ecbdf890ff108083
atharvdesai1996 <49723941+atharvdesai1996@users.noreply.github.com>
2019-11-13 22:09:37 -0700

    All circ buffer functions with realloc (except valid ptr fn) + error handling

--- ASSIGNMENT5/source/circbuff.c
+++ ASSIGNMENT5/source/circbuff.c
@@ -15,34 +15,140 @@ errorcheck init_buffer(circbuffstruct * ptr, uint8_t buffersize)
 			ptr->head = ptr->buffer;
 			ptr->tail = ptr->buffer;
 			ptr->maxlength = buffersize;
-			ptr->count = 0;
+			ptr->countofdata = 0;
 			return buffer_init_pass;
 		}
 	}
 
-/******************** Function to add data to buffer***********************/
+/********************************* Function to add data to buffer***********************************/
 
-errorcheck add_to_buffer(circbuffstruct* ptr,uint8_t data)
+errorcheck add_to_buffer(circbuffstruct* ptr,uint8_t newelement)
 {
 	if(ptr ==NULL)
 		return buffer_init_fail;
-	else if(ptr->count ==ptr->maxlength)
-		return addtobuffer_fail;
+	else if(ptr->countofdata ==ptr->maxlength)
+	{
+		/* Call realloc Function*******************/
+		ptr->head = ptr->buffer;
+		*ptr->head =newelement;  //data overwriting starts#########
+		ptr->countofadds++;
+		ptr->countofdata++;
+		return bufferfull_pass;
+	}
 	else if(ptr->head == ptr->buffer + ptr->maxlength -1)   // ptr-> count == ptr-> maxlength -1
 	{
-		*ptr->head =data;            // using head to write the data
+		*ptr->head =newelement;            // using head to write the data
 		ptr->head = ptr->buffer;   // to bring head back to starting position
-		ptr->count++;
-		return wrap_flag_add;
+		ptr->countofadds++;
+		ptr->countofdata++;
+		return wrap_flag_add;      // Data written on wrap around flag
 	}
 	else
 	{
-		*ptr ->head =data;
+		*ptr ->head =newelement;
 		ptr->head++;
-		ptr->count++;
+		ptr->countofdata++;
+		ptr->countofadds++;
 		return addtobuffer_pass;
 	}
 }
 
 
-/***********************Remove data from buffer****************************/
+/*********************************Remove data from buffer*****************************************/
+
+errorcheck rem_data_from_buffer(circbuffstruct* ptr,uint8_t remelement)
+{
+	if(ptr==NULL)
+		return buffer_init_fail;                             // to check if buffer isnt initialized
+	else if (ptr->countofremoves <= ptr->countofadds)
+	{
+		if (ptr->tail == ptr->buffer + ptr->maxlength -1)   // wrap around byte removal condition
+		{
+		remelement = *ptr->tail;
+		ptr->tail = ptr->buffer;
+		ptr->countofdata--;
+		ptr->countofremoves++;
+		return wrap_flag_remove;
+		}
+		else                                               // general condition for remove element
+		{
+		remelement = *ptr->tail;
+	    ptr->tail++;
+		ptr->countofdata--;
+		ptr->countofremoves++;
+		return remfrombuffer_pass;
+		}
+	}
+	else
+	{
+		printf(" Error: Trying to read more data than written in buffer");
+		return remfrombuffer_fail;
+	}
+}
+
+
+/*********************************Check if buffer full********************************************/
+
+errorcheck is_buffer_full(circbuffstruct* ptr)
+{
+	if(ptr==NULL)
+		return buffer_init_fail;
+	else if (ptr->countofdata == ptr->maxlength)
+		return bufferfull_pass;
+	/************Call realloc function**************/
+	else
+		return bufferfull_fail;
+}
+
+/*********************************Check if buffer empty**************************************************************/
+
+errorcheck is_buffer_empty(circbuffstruct* ptr)
+{
+	if(ptr==NULL)
+		return buffer_init_fail;
+	else if (ptr->head == ptr->buffer)    //ptr->count == 0
+		return buffer_empty_pass;
+	else
+		return buffer_empty_fail;
+
+}
+
+/*********************************Destroy a buffer**********************************************************************/
+errorcheck buffer_destroy(circbuffstruct* ptr)
+{
+	if(ptr==NULL)
+	{
+		return buffer_delete_fail;
+	}
+	else
+	{
+		free(ptr->buffer);
+		return buffer_delete_pass;
+	}
+}
+
+/**************************************Buffer initialize verify***********************************************************/
+errorcheck verify_buffer_init(circbuffstruct * ptr, uint8_t buffersize)
+
+{
+	if(ptr->buffer == NULL || buffersize < 0)
+		{
+			return buffer_init_fail;
+		}
+		else
+		{
+			return buffer_init_pass;
+		}
+}
+
+/**************************************Reallocate a circular buffer on overflow*******************************************/
+errorcheck realloc_buffer(circbuffstruct * ptr)
+
+{
+	    ptr->buffer = realloc(ptr->buffer,ptr->maxlength*2);
+	    ptr->maxlength = (ptr->maxlength)*2;
+	    if (ptr->buffer == NULL)
+	    	return buffer_realloc_fail;
+	    else
+	    	return buffer_realloc_pass;
+}
--- ASSIGNMENT5/source/circbuff.h
+++ ASSIGNMENT5/source/circbuff.h
@@ -11,6 +11,7 @@ typedef enum errorcheck
 	addtobuffer_pass=0,
 	addtobuffer_fail,
 	wrap_flag_add,
+	wrap_flag_remove,
 	remfrombuffer_pass,
 	remfrombuffer_fail,
 	bufferfull_pass,
@@ -23,21 +24,30 @@ typedef enum errorcheck
 	buffer_init_fail,
 	buffer_delete_pass,
 	buffer_delete_fail,
+	buffer_realloc_pass,
+	buffer_realloc_fail
 }errorcheck;
 
 typedef struct {                 //create structure for circular buffer
-    uint8_t *buffer;      // pointer to a buffer
+    uint8_t *buffer;             // pointer to a buffer
     uint8_t *head;               // buffer head used for write
-    uint8_t *tail; 				// buffer tail used for read
-    uint8_t maxlength;        // Maximum length of buffer
-    uint8_t count;              // Count of data occupied by buffer
+    uint8_t *tail; 				 // buffer tail used for read
+    uint8_t maxlength;           // Maximum length of buffer
+    uint8_t countofdata;               // Count of data occupied by buffer
+    uint8_t countofadds;         // count of no. of times data added
+    uint8_t countofremoves;      // count of no. of times data removed
 } circbuffstruct;
 
 
 // Function declaration
 errorcheck init_buffer(circbuffstruct *ptr, uint8_t buffersize);
 errorcheck add_to_buffer(circbuffstruct *ptr, uint8_t newelement);
-
+errorcheck is_buffer_full(circbuffstruct* ptr);
+errorcheck is_buffer_empty(circbuffstruct* ptr);
+errorcheck rem_data_from_buffer(circbuffstruct* ptr,uint8_t remelement);
+errorcheck buffer_destroy(circbuffstruct* ptr);
+errorcheck verify_buffer_init(circbuffstruct * ptr, uint8_t buffersize);
+errorcheck realloc_buffer(circbuffstruct * ptr);
 
 
 
